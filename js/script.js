40bb0b029148c602a0b8e260f3964a711568d77b
Karma King <2114747@talnet.nl>
2019-11-06 12:17:50 +0100

    like button met counter gemaakt YEEEET

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 var dt = new Date();
 document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "." + (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (dt.getFullYear()) + " " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2));
 ``
+
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    }
+}
